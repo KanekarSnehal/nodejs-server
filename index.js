6e050204020abf32eaa522e621e11f5f6ea5bed8
KanekarSnehal <snehalkanekar43@gmail.com>
2021-11-17 15:05:48 +0530

    understanding request

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
+    // process.exit();
 });
 
 server.listen(4000);
