d655fef277b64a5f6e3b6e4c59a9612a02961b30
KanekarSnehal <snehalkanekar43@gmail.com>
2021-11-20 16:09:13 +0530

    Debugging Techniques

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ if (url === '/message' && method === 'POST') {
     });
     return req.on('end', () => {
         const parseBody = Buffer.concat(body).toString();
-        const message = parseBody.split('=')[1];
+        const message = parseBody.split('=')[0];
 
         fs.writeFile('message.txt', message, (error) => {
             res.statusCode = 302;
