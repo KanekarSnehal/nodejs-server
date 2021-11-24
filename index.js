b77e49a4660a8a31933c47b9d3b95aa815bfb3dc
KanekarSnehal <snehalkanekar43@gmail.com>
2021-11-24 15:47:45 +0530

    Intro To Middlewares

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const http = require('http');
-const routes=require('./routes');
-console.log(routes.someText);
-const server=http.createServer(routes.handler);
+// const http = require('http');
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
 
-server.listen(4000);
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log("this always runs");
+    // res.send('<h1>hello</h1>');
+    next();
+})
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("in middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
+})
+app.get('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    console.log("in middleware");
+    res.send('<h1>hello</h1>');
+    
+})
+
+// const server=http.createServer(app);
+
+app.listen(4000);
